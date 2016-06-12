c70c806bb706cc082146654de74e5dae72b3c67d
avidnyat <avidnyat@gmail.com>
2016-06-12 23:16:27 +0530

    div chages

--- app/calendarComponent.js
+++ app/calendarComponent.js
@@ -28,6 +28,10 @@ const CalendarComponent = React.createClass({
     $(".rc-calendar-table").find("th").each(function(i) {
         $(this).html("<span class='rc-calendar-column-header'><input type='checkbox'>"+$(this).prop("title")+"</span>");
     });
+    
+$(".rc-calendar-table").find("td").each(function(i) {
+     $(this).html('<div class="rc-calendar-date"><a class="link" href="#">Edit</a><p>5</p><div class="gray-section"><div class="occupied"><span>70</span>Occupied</div><div class="total"><span>70</span>Total seats</div></div><div class="gray-section2"><div class="total">Total seats <input type="text" ></div></div></div>');
+    });
 
   },
   render() {
